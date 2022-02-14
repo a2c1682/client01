766ccdacaa492fd57a19e39f710409e1cf408ca8
a2c1682 <taurustechnologies1682@gmail.com>
2022-02-14 18:59:10 +0900

    ローディング画面のアニメーション処理①

---
+++ public/javascripts/index.js
@@ -0,0 +1,33 @@
+//SVGの初期設定
+var logoVivus1;
+
+//#logoのSVG初期設定
+function VivusInit() {
+	logoVivus1 = new Vivus('logo', 
+		{
+			start: 'autostart',
+			duration: 100,
+			type: 'scenario',
+			pathTimingFunction: Vivus.EASE,
+		},
+		function(obj) {
+			$("#logo").attr("class", "done");
+		}
+	);
+	logoVivus1.stop();
+}
+
+//スクロールで#logoのSVGが出現する設定
+function VivusAnime() {
+	var elemPos = $("#logo").offset().top-50;
+	var scroll = $(window).scrollTop();
+	var windowHeight = $(window).height();
+	if (scroll >= elemPos - windowHeight) {
+		logoVivus1.play(1);
+	}
+}
+
+$(window).on('load', function() {
+	VivusInit();
+	VivusAnime();
+});

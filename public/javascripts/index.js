3e0f8140f0021ea0547b5f646a1ead0063a78a78
a2c1682 <taurustechnologies1682@gmail.com>
2022-02-20 12:55:29 +0900

    コンテンツ区切りの波を実装

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -104,3 +104,83 @@ function delayScrollAnime() {
 $(window).scroll(function() {
 	delayScrollAnime();
 });
+
+//波：コンテンツ区切り1
+var unit = 100,
+	canvasList,
+	info = {},
+	colorList;
+
+function init() {
+	info.seconds = 0;
+	info.t = 0;
+	canvasList = [];
+	colorList = [];
+
+	canvasList.push(document.getElementById("waveCanvas"));
+	colorList.push(['#0ff', '#ff0', '#f00', '#00f', '#f0f']);
+	canvasList.push(document.getElementById("waveCanvas2"));
+	colorList.push(['#43c0e4']);
+	canvasList.push(document.getElementById("waveCanvas3"));
+	colorList.push(['#666', '#888']);
+
+	for (var canvasIndex in canvasList) {
+		var canvas = canvasList[canvasIndex];
+		canvas.width = document.documentElement.clientWidth;
+		canvas.height = 200;
+	
+	canvas.contextCache = canvas.getContext("2d");
+	}
+
+	update();
+}
+
+function update() {
+	for (var canvasIndex in canvasList) {
+		var canvas = canvasList[canvasIndex];
+		draw(canvas, colorList[canvasIndex]);
+	}
+
+	info.seconds = info.seconds + .014;
+	info.t = info.seconds * Math.PI;
+	setTimeout(update, 35);
+}
+
+function draw(canvas, color) {
+	var context = canvas.contextCache;
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	drawWave(canvas, color[0], 0.8, 3, 0);
+	drawWave(canvas, color[1], 0.5, 4, 0);
+	drawWave(canvas, color[2], 0.3, 1.6, 0);
+	drawWave(canvas, color[3], 0.2, 3, 100);
+	drawWave(canvas, color[4], 0.5, 1.6, 250);
+}
+
+function drawWave(canvas, color, alpha, zoom, delay) {
+	var context = canvas.contextCache;
+	context.strokeStyle = color;
+	context.lineWidth = 1;
+	context.globalAlpha = alpha;
+	context.beginPath();
+	drawSine(canvas, info.t / 0.5, zoom, delay);
+	context.stroke();
+}
+
+function drawSine(canvas, t, zoom, delay) {
+	var xAxis = Math.floor(canvas.height / 2);
+	var yAxis = 0;
+	var context = canvas.contextCache;
+	var x = t;
+	var y = Math.sin(x) / zoom;
+	context.moveTo(yAxis, unit * y + xAxis);
+	
+	for (i = yAxis; i <= canvas.width + 10; i += 10) {
+		x = t + (-yAxis + i) / unit / zoom;
+		y = Math.sin(x - delay) / 3;
+		context.lineTo(i, unit * y + xAxis);
+	}
+}
+
+init();
+
